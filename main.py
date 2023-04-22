fea7c0d524ecd1d95b742d653e598cb5889c29b9
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-04-22 15:07:40 -0500

    🦄 [ADD] comments 🦄

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tools.tools import get_songs
 if 'selected_song' not in st.session_state:
     st.session_state.selected_song = None
     
-# Get the songs from airtable
+# Get the songs from airtable and store as a session state so we do not lose the data in each interaction
 if 'songs' not in st.session_state:
 	st.session_state.songs = get_songs()
 
@@ -14,27 +14,37 @@ if 'songs' not in st.session_state:
 # Define the main page:
 if st.session_state.selected_song is None:
 	# Note: You can write with MAGIC!
+	# Write the header of our page
 	st.write("""
 	# Mood Melodies 🎶
 	A streamlit web page for analize the sentiment of lyrics!
 	""")
 
+	# Create the columns to display the cards
 	cols = st.columns(2)
+	# Check when the user click the cards
 	clicked_cards = [False] * len(st.session_state.songs)
+	# Create the cards in each column from the session state songs
 	for index, song in enumerate(st.session_state.songs):
 		with cols[index % 2]:
+			# Store the click state of each card
 			clicked_cards[index] = card(
 				title=song['song_name'],
 				text=song['artist_name'],
 				image=song['img'],
 				key = index,
 			)
-
+	# Check the clicked cards (for some reason we need to do double click)
 	for index, clicked in enumerate(clicked_cards):
 		if clicked:
 			st.session_state.selected_song = st.session_state.songs[index]
-else:
+else: # Define the secondary page where it is suppose to be all the song information and sentiment analysis
+	# Define the back button function
 	def update_selected_song():
 		st.session_state.selected_song = None
-	st.session_state.selected_song
 	st.button('👈 back', on_click=update_selected_song)
+	
+	# Here is all the structure
+	st.session_state.selected_song
+	
+	
--- tools/tools.py
+++ tools/tools.py
@@ -1,14 +1,18 @@
 import requests as req
 
 def get_songs():
-    # Define 
+    # Define the haeaders for the search
+	# Remember to change to secrets that bearer (and re-generate it)
 	headers = {
 		'Authorization': 'Bearer patz6X6tWMIc2WX9W.b9ff32c0815ae279a224ca8454e69065b92252b7576c470f30c680fae05b54ab'
 	}
+	# Get the url
 	url = 'https://api.airtable.com/v0/appSU80hu0LcLPPg3/Lyrics%20Sentimental%20Analysis?'
+	# Get the data from requests
 	res = req.get(url, headers=headers)
-	records = res.json()['records']
+	records = res.json()['records'] # Get records
 
+	# Map out all the fields instead of all the object
 	songs = list(map(lambda record: record['fields'], records))
 
 	return songs
